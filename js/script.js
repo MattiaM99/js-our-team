618d092c50116f7117777d71b77065bb9a450288
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-04 15:36:48 +0100

    creato array di oggetti

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const char = [
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    img: 'img\angela-caroll-chief-editor.jpg'
+  },
+  {
+    nome:'Walter Gordon',
+    ruolo:'Office Manager',
+    img:'img\walter-gordon-office-manager.jpg'
+  },
+  {
+    nome:'Angela Lopez',
+    ruolo:'Social Media Manager',
+    img:'img\angela-lopez-social-media-manager.jpg'
+  },
+  {
+    nome:'Scott Estrada',
+    ruolo:'Developer',
+    img:'img\scott-estrada-developer.jpg'
+  },
+  {
+    nome:'Barbara Ramos',
+    ruolo:'graphic Designer',
+    img:'img\barbara-ramos-graphic-designer.jpg'
+  }
+]
+
+console.log(char);
+
+// const nuovoChar = {
+//   nome: prompt('inserisci nome'),
+//   ruolo: prompt('inserisci ruolo'),
+//   img: prompt('inserisci url immagine') 
+// };
+
+// char.push(nuovoChar);
+
