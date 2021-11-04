72b94d041693b0608304909d7b2c82bee2a2e86f
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-04 15:51:23 +0100

    creato ciclo per inserire tutte le immagini nell'html, trovare l'alternativa al prompt per inserimento di nuovi profili

--- js/script.js
+++ js/script.js
@@ -1,38 +1,64 @@
+
 const char = [
   {
     nome: 'Angela Caroll',
     ruolo: 'Chief Editor',
-    img: 'img\angela-caroll-chief-editor.jpg'
+    img: 'img/angela-caroll-chief-editor.jpg'
   },
   {
     nome:'Walter Gordon',
     ruolo:'Office Manager',
-    img:'img\walter-gordon-office-manager.jpg'
+    img:'img/walter-gordon-office-manager.jpg'
   },
   {
     nome:'Angela Lopez',
     ruolo:'Social Media Manager',
-    img:'img\angela-lopez-social-media-manager.jpg'
+    img:'img/angela-lopez-social-media-manager.jpg'
   },
   {
     nome:'Scott Estrada',
     ruolo:'Developer',
-    img:'img\scott-estrada-developer.jpg'
+    img:'img/scott-estrada-developer.jpg'
   },
   {
     nome:'Barbara Ramos',
     ruolo:'graphic Designer',
-    img:'img\barbara-ramos-graphic-designer.jpg'
+    img:'img/barbara-ramos-graphic-designer.jpg'
   }
 ]
 
+const container = document.querySelector(".team-container");
+
+
 console.log(char);
 
-// const nuovoChar = {
-//   nome: prompt('inserisci nome'),
-//   ruolo: prompt('inserisci ruolo'),
-//   img: prompt('inserisci url immagine') 
-// };
+const nuovoChar = {
+  nome: prompt('inserisci nome'),
+  ruolo: prompt('inserisci ruolo'),
+  img: prompt('inserisci url immagine') 
+};
 
-// char.push(nuovoChar);
+char.push(nuovoChar);
 
+
+for(let index in char){
+  const newChar = char[index];
+  const newName = newChar.nome;
+  const newRole = newChar.ruolo;
+  const newImg = newChar.img;
+
+
+  container.innerHTML += `
+  <div class="team-card">
+    <div class="card-image">
+      <img
+        src="${newImg}"/>
+      </div>
+      <div class="card-text">
+      <h3>${newName}</h3>
+      <p>${newRole}</p>
+    </div>
+  </div>
+`
+;
+};
